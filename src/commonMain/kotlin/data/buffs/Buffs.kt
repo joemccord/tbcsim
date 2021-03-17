cde367e7b184aa3c1258c1138206965b8438a78d
Requiel <72976375+joemccord@users.noreply.github.com>
2021-03-17 19:55:03 +1000

    Revert "Added Abacus and Icon on-use buff"

--- src/commonMain/kotlin/data/buffs/Buffs.kt
+++ src/commonMain/kotlin/data/buffs/Buffs.kt
@@ -107,8 +107,6 @@ object Buffs {
             35166 -> BloodlustBrooch()
             39438 -> DarkmoonCardCrusadeAP()
             39440 -> DarkmoonCardCrusadeSP()
-            33807 -> AbacusOfViolentOdds()
-            34106 -> IconOfUnyieldingCourage()
 
             // All the random mp5 buffs
             18378 -> GenericManaRegenBuff(8)
